103528dab1ef4297b319413b7eb00007d97824ac
Thykof <thykof@protonmail.ch>
2023-12-12 18:48:44 -0500

    add footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,12 +16,20 @@ export const Home: React.FC = () => {
   return (
     <div>
       <h1 className="p-4 text-5xl text-primary font-bold text-center">Welcome to the language coach!</h1>
-      <div className="flex justify-center">
+      <div className="flex justify-center pb-6">
         <button className={`btn ${startActive} btn-primary`} onClick={start}>
           Start
         </button>
         {session && <Chat session={session} />}
       </div>
+      <footer className="flex flex-row justify-around p-4 bg-neutral text-neutral-content">
+        <p>
+          Powered by <a href="https://openai.com">OpenAI</a>
+        </p>
+        <p>
+          Make with &lt;3 by <a href="https://github/thykof">Nathan!</a>
+        </p>
+      </footer>
     </div>
   );
 };
