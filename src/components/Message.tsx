392e895b6331645f3cdcc86f6d46dd1e0ecef77c
Thykof <thykof@protonmail.ch>
2023-12-13 14:37:19 -0500

    replay speech

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Message, Roles } from '../service/types';
+import { PlayCircle } from './icons/PlayCircle';
+import { play } from '../service/utils';
 
 interface MessageProps {
   message: Message;
@@ -9,9 +11,20 @@ export const MessageComponent: React.FC<MessageProps> = ({ message }) => {
   const chatClass = message.role === Roles.User ? 'chat-end' : 'chat-start';
   const chatBubbleClass = message.role === Roles.User ? 'chat-bubble-primary' : 'chat-bubble-accent';
 
+  const replay = () => {
+    play(message.speech?.data!);
+  };
+
   return (
     <div className={`chat ${chatClass}`}>
       <div className={`chat-bubble ${chatBubbleClass}`}>{message.content}</div>
+      {message.speech && (
+        <div className="chat-footer opacity-50 p-1">
+          <button onClick={replay}>
+            <PlayCircle />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
